54c577976e562f08f8400487be72e0224205036b
Khoa Dang Quoc <khoadangq@nashtechglobal.com>
2023-08-04 21:59:34 +0700

    install docker successfully

--- jenkins_docker/install_jenkins_docker.sh
+++ jenkins_docker/install_jenkins_docker.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+sudo amazon-linux-extras install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+
 sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
 # Add required dependencies for the jenkins package
-sudo yum install java-17-openjdk -y
+sudo amazon-linux-extras install java-openjdk11 -y
 sudo yum install jenkins -y
 sudo systemctl daemon-reload
 sudo systemctl enable jenkins
